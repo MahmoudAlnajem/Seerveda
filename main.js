f10a2ba28a98bc358baaf2816a47157fd2e95acb
MahmoudAlnajem <alnjm7023lap@gmail.com>
2023-10-01 18:54:53 +0300

    add the managment section

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// start slide nav 
+// start slide nav
 
 let BurgerLinks = document.querySelector(".burger-links");
 
@@ -19,7 +19,7 @@ BurgerLinks.addEventListener("click", () => {
   }
 });
 
-// end slide nav 
+// end slide nav
 
 // start image slider
 
@@ -34,16 +34,16 @@ let nextbutton = document.querySelector(".navigation-angles i:first-child");
 let prevbutton = document.querySelector(".navigation-angles i:last-child");
 
 // ***********************************
-let first ;
+let first;
 
-function set(){
-    first = setInterval(() => {
+function set() {
+  first = setInterval(() => {
     current++;
     if (current === imagesArr.length) {
       current = 0;
     }
     removeAll();
-  
+
     imagesArr[current].classList.add("active");
     bulletsArr[current].classList.add("active");
   }, 3000);
@@ -51,9 +51,8 @@ function set(){
 
 set();
 
-
 nextbutton.onclick = function () {
-  clearInterval(first)
+  clearInterval(first);
   current++;
   if (current === imagesArr.length) {
     current = 0;
@@ -66,9 +65,8 @@ nextbutton.onclick = function () {
   set();
 };
 
-
 prevbutton.onclick = function () {
-  clearInterval(first)
+  clearInterval(first);
   current--;
   if (current < 0) {
     current = 0;
@@ -82,8 +80,6 @@ prevbutton.onclick = function () {
 };
 // ***********************************
 
-
-
 function removeAll() {
   imagesArr.forEach((img) => {
     img.classList.remove("active");
@@ -95,3 +91,108 @@ function removeAll() {
 }
 
 // end image slide
+
+// start managment section
+
+async function generateCardsDependsOnRespnse() {
+  let response = await fetch("jsonFiles/managment.json").then((r) => {
+    let answer = r.json();
+    return answer;
+  }).then((r) => r["management"]);
+
+
+  for(val of response){
+    let swiperWrapper = document.querySelector(".swiper-wrapper");
+    let card  = document.createElement("div");
+    card.classList.add("card")
+    card.classList.add("swiper-slide")
+
+    swiperWrapper.appendChild(card);
+
+    // start to the card body
+
+    // start card image 
+
+    let cardImage = document.createElement("div")
+
+    cardImage.classList.add("card__image")
+
+    let cardImageImg = document.createElement("img")
+
+    cardImageImg.setAttribute("src",val.image)
+
+    cardImage.appendChild(cardImageImg)
+
+    card.appendChild(cardImage)
+
+    // start card content 
+
+    let cardContent = document.createElement("div")
+
+    cardContent.classList.add("card__content")
+
+    // start card title 
+
+    let cardTitle = document.createElement("span")
+
+    cardTitle.classList.add("card__title")
+
+    cardTitle.innerHTML = val.title
+
+    cardContent.appendChild(cardTitle)
+
+    // start card name
+
+    let cardName = document.createElement("span")
+
+    cardName.classList.add("card__name")
+
+    cardName.innerHTML = val.name
+
+    cardContent.appendChild(cardName)
+
+    // start card text
+
+    let cardText = document.createElement("p")
+
+    cardText.classList.add("card__text")
+
+    cardText.innerHTML = val.text 
+
+    cardContent.appendChild(cardText)    
+
+    // start card phone 
+
+    let cardPhone = document.createElement("p")
+
+    cardPhone.classList.add("card__phone")
+
+    cardPhone.innerHTML = `Phone: ${val.phone}`  
+
+    cardContent.appendChild(cardPhone) 
+
+    // start card email 
+
+    let cardEmail = document.createElement("p")
+
+    cardEmail.classList.add("card__email")
+
+    cardEmail.innerHTML = `Email: ${val.phone}`  
+
+    cardContent.appendChild(cardEmail) 
+
+    // start button 
+
+    let cardButton = document.createElement("button")
+
+    cardButton.classList.add("card__btn")
+
+    cardButton.innerHTML = `VIEW MORE`
+
+    cardContent.appendChild(cardButton) 
+
+    card.appendChild(cardContent)
+  }
+}
+generateCardsDependsOnRespnse();
+// // start managment section
