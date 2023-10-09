8704e437187c5879e7dce10e000c683042c4efed
MahmoudAlnajem <alnjm7023lap@gmail.com>
2023-10-09 21:59:34 +0300

    Huge Edition By Mahmoud

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // start slide nav
-let landingSec = document.querySelector(".landing-sec");
+
 let BurgerLinks = document.querySelector(".burger-links");
 
 BurgerLinks.addEventListener("click", () => {
@@ -13,11 +13,9 @@ BurgerLinks.addEventListener("click", () => {
   if (BurgerLinks.classList.contains("clicked")) {
     slideNav.style.left = "0";
     header.classList.add("hoverd");
-    landingSec.style.filter = " blur(7px) brightness(0.5)";
   } else {
     slideNav.style.left = "-1000px";
     header.classList.remove("hoverd");
-    landingSec.style.filter = " blur(0px) brightness(1)";
   }
 });
 
@@ -53,7 +51,7 @@ function set() {
 
 set();
 
-nextbutton.onclick = function() {
+nextbutton.onclick = function () {
   clearInterval(first);
   current++;
   if (current === imagesArr.length) {
@@ -67,7 +65,7 @@ nextbutton.onclick = function() {
   set();
 };
 
-prevbutton.onclick = function() {
+prevbutton.onclick = function () {
   clearInterval(first);
   current--;
   if (current < 0) {
@@ -83,13 +81,118 @@ prevbutton.onclick = function() {
 // ***********************************
 
 function removeAll() {
-  imagesArr.forEach(img => {
+  imagesArr.forEach((img) => {
     img.classList.remove("active");
   });
 
-  bulletsArr.forEach(p => {
+  bulletsArr.forEach((p) => {
     p.classList.remove("active");
   });
 }
 
 // end image slide
+
+// start managment section
+
+async function generateCardsDependsOnRespnse() {
+  let response = await fetch("jsonFiles/managment.json")
+    .then((r) => {
+      let answer = r.json();
+      return answer;
+    })
+    .then((r) => r["management"]);
+
+  for (val of response) {
+    let swiperWrapper = document.querySelector(".swiper-wrapper");
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("swiper-slide");
+
+    swiperWrapper.appendChild(card);
+
+    // start to the card body
+
+    // start card image
+
+    let cardImage = document.createElement("div");
+
+    cardImage.classList.add("card__image");
+
+    let cardImageImg = document.createElement("img");
+
+    cardImageImg.setAttribute("src", val.image);
+
+    cardImage.appendChild(cardImageImg);
+
+    card.appendChild(cardImage);
+
+    // start card content
+
+    let cardContent = document.createElement("div");
+
+    cardContent.classList.add("card__content");
+
+    // start card title
+
+    let cardTitle = document.createElement("span");
+
+    cardTitle.classList.add("card__title");
+
+    cardTitle.innerHTML = val.title;
+
+    cardContent.appendChild(cardTitle);
+
+    // start card name
+
+    let cardName = document.createElement("span");
+
+    cardName.classList.add("card__name");
+
+    cardName.innerHTML = val.name;
+
+    cardContent.appendChild(cardName);
+
+    // start card text
+
+    let cardText = document.createElement("p");
+
+    cardText.classList.add("card__text");
+
+    cardText.innerHTML = val.text;
+
+    cardContent.appendChild(cardText);
+
+    // start card phone
+
+    let cardPhone = document.createElement("p");
+
+    cardPhone.classList.add("card__phone");
+
+    cardPhone.innerHTML = `Phone: ${val.phone}`;
+
+    cardContent.appendChild(cardPhone);
+
+    // start card email
+
+    let cardEmail = document.createElement("p");
+
+    cardEmail.classList.add("card__email");
+
+    cardEmail.innerHTML = `Email: ${val.phone}`;
+
+    cardContent.appendChild(cardEmail);
+
+    // start button
+
+    let cardButton = document.createElement("button");
+
+    cardButton.classList.add("card__btn");
+
+    cardButton.innerHTML = `VIEW MORE`;
+
+    cardContent.appendChild(cardButton);
+
+    card.appendChild(cardContent);
+  }
+}
+generateCardsDependsOnRespnse();
